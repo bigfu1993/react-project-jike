cf26e1fc8a5755446bdb26f8916c5850ecf2c356
bigfu@home-pc <bigfu1993@163.com>
2024-04-14 21:19:21 +0800

    form data

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -5,16 +5,22 @@ import logo from '@/assets/logo.png'
 
 
 const Login = () => {
+    const handleFormSubmit = (values)=>{
+        console.log(values)
+    }
     return (
         <div className="login">
             <Card className="login-container">
                 <img className="login-logo" src={logo} alt="" />
                 {/* 登录表单 */}
-                <Form>
-                    <Form.Item name="name" label="手机号" rules={[{ required: true, message: '请输入手机号' }]}>
+                <Form validateTrigger="onBlur" onFinish={handleFormSubmit}>
+                    <Form.Item name="name" label="手机号" rules={[
+                        { required: true, message: '请输入手机号' },
+                        { pattern: /^1[3-9]\d{9}$/,message:'请输入正确的手机格式' }
+                    ]}>
                         <Input size="large" placeholder="请输入手机号" />
                     </Form.Item>
-                    <Form.Item name="pwd" label="手机号" rules={[{ required: true, message: '请输入验证码' }]}>
+                    <Form.Item name="pwd" label="密码" rules={[{ required: true, message: '请输入验证码' }]}>
                         <Input size="large" placeholder="请输入验证码" />
                     </Form.Item>
                     <Form.Item>
