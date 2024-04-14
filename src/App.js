817505f77060c3776cf9b96a1e61073e62a1e1a9
bigfu@home-pc <bigfu1993@163.com>
2024-04-15 00:59:04 +0800

    store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import { Button } from 'antd';
+import { useSelector } from 'react-redux'
 
 function App() {
+  const user = useSelector(state => state.user)
   return (
     <div className="App">
-     app
-     <Button type="primary">Button</Button>
+      app : { user }
+      <Button type="primary">Button</Button>
     </div>
   );
 }
--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,5 +1,10 @@
-export default function Layout(){
+import { useSelector } from 'react-redux'
+
+export default function Layout() {
+    const { token } = useSelector(state => state.user)
     return (
-        <div>layout</div>
+        <div>
+            layout : {token}
+        </div>
     )
 }
