0a209572cb3977c65b66495f01ae8eccfcbbf0a1
bigfu@home-pc <bigfu1993@163.com>
2024-04-14 20:26:39 +0800

    form verify

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,31 @@
-export default function Login() {
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+// const logo = require('@/assets/logo.png')
+
+
+const Login = () => {
     return (
-        <div>login</div>
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                {/* 登录表单 */}
+                <Form>
+                    <Form.Item name="name" label="手机号" rules={[{ required: true, message: '请输入手机号' }]}>
+                        <Input size="large" placeholder="请输入手机号" />
+                    </Form.Item>
+                    <Form.Item name="pwd" label="手机号" rules={[{ required: true, message: '请输入验证码' }]}>
+                        <Input size="large" placeholder="请输入验证码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
     )
 }
+
+export default Login
