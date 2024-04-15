dbc108129d883c99e59777520f128960a0279c0b
bigfu@home-pc <bigfu1993@163.com>
2024-04-15 10:20:28 +0800

    token持久化

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,7 @@
 import './index.scss'
-import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
+import { Card, Form, Input, Button, message } from 'antd'
+import {useNavigate} from 'react-router-dom'
 import { fetchLogin } from '@/store/modules/userStore'
 import { useDispatch } from 'react-redux'
 // const logo = require('@/assets/logo.png')
@@ -8,9 +9,12 @@ import { useDispatch } from 'react-redux'
 
 const Login = () => {
     const dispatch = useDispatch()
-    const handleFormSubmit = (foramData)=>{
+    const navigate = useNavigate()
+    const handleFormSubmit = async (foramData) => {
         console.log(foramData)
-        dispatch(fetchLogin(foramData))
+        await dispatch(fetchLogin(foramData))
+        navigate('/')
+        message.success('登陆成功')
     }
     return (
         <div className="login">
@@ -18,14 +22,14 @@ const Login = () => {
                 <img className="login-logo" src={logo} alt="" />
                 {/* 登录表单 */}
                 <Form validateTrigger="onBlur" onFinish={handleFormSubmit}>
-                    <Form.Item name="name" label="手机号" rules={[
+                    <Form.Item name="mobile" label="手机号" rules={[
                         { required: true, message: '请输入手机号' },
-                        { pattern: /^1[3-9]\d{9}$/,message:'请输入正确的手机格式' }
+                        { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机格式' }
                     ]}>
-                        <Input size="large" placeholder="请输入手机号" />
+                        <Input size="large" defaultValue={13800000002} placeholder="请输入手机号" />
                     </Form.Item>
-                    <Form.Item name="pwd" label="密码" rules={[{ required: true, message: '请输入验证码' }]}>
-                        <Input size="large" placeholder="请输入验证码" />
+                    <Form.Item name="code" label="密码" rules={[{ required: true, message: '请输入验证码' }]}>
+                        <Input size="large" defaultValue={246810} placeholder="请输入验证码" />
                     </Form.Item>
                     <Form.Item>
                         <Button type="primary" htmlType="submit" size="large" block>
--- src/store/modules/userStore.js
+++ src/store/modules/userStore.js
@@ -1,28 +1,59 @@
-import { request } from '@/apis'
+// 和用户相关的状态管理
 import { createSlice } from '@reduxjs/toolkit'
+import { setToken as _setToken, getToken, removeToken } from '@/apis/token'
+import { loginAPI, getProfileAPI } from '@/apis/modules/user'
 
 const userStore = createSlice({
-    name: 'userStore',
+    name: "user",
+    // 数据状态
     initialState: {
-        token: 'abc'
+        token: getToken() || '',
+        userInfo: {}
     },
+    // 同步修改方法
     reducers: {
-        setToken(state, actions) {
-            state.token = actions.payload
+        setToken(state, action) {
+            state.token = action.payload
+            _setToken(action.payload)
+        },
+        setUserInfo(state, action) {
+            state.userInfo = action.payload
+        },
+        clearUserInfo(state) {
+            state.token = ''
+            state.userInfo = {}
+            removeToken()
         }
     }
 })
 
-const { setToken } = userStore.actions
 
+// 解构出actionCreater
+
+const { setToken, setUserInfo, clearUserInfo } = userStore.actions
+
+// 获取reducer函数
+
+const userReducer = userStore.reducer
+
+// 登录获取token异步方法封装
 const fetchLogin = (loginForm) => {
     return async (dispatch) => {
-        const res = await request.post('/authorizations', loginForm)
-        dispatch(setToken(res.data.token))
+        // const res = await loginAPI(loginForm)
+        // dispatch(setToken(res.data.token))
+        
+        dispatch(setToken('123'))
     }
 }
 
-const reducer = userStore.reducer
+// 获取个人用户信息异步方法
+const fetchUserInfo = () => {
+    return async (dispatch) => {
+        const res = await getProfileAPI()
+        dispatch(setUserInfo(res.data))
+    }
+}
 
-export { setToken, fetchLogin }
-export default reducer
+export { fetchLogin, fetchUserInfo, clearUserInfo }
+
+export default userReducer
