1337312d0c2c4b4f8371fdbdc0ce91592a38b7d7
bigfu@home-pc <bigfu1993@163.com>
2024-04-16 00:02:15 +0800

    publish channels

--- src/pages/publish/index.js
+++ src/pages/publish/index.js
@@ -11,15 +11,29 @@ import {
 } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 import './index.scss'
+import { useEffect, useState } from 'react'
+import { getChannelAPI } from '@/apis/modules/article'
 
 const { Option } = Select
 
 const Publish = () => {
+    useEffect(() => {
+        async function fetchChannels() {
+            let res = await getChannelAPI()
+            setChannels(res.data.channels)
+        }
+        fetchChannels()
+    }, [])
+
     const breadItems = [
         { title: <Link to={'/'}>首页</Link> },
         { title: '发布文章' }
     ]
+    const [channels, setChannels] = useState([])
+
     return (
         <div className="publish">
             <Card title={<Breadcrumb items={breadItems} />} >
@@ -41,14 +55,20 @@ const Publish = () => {
                         rules={[{ required: true, message: '请选择文章频道' }]}
                     >
                         <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-                            <Option value={0}>推荐</Option>
+                            {channels.map(ch => <Option key={ch.id} value={ch.id}>{ch.name}</Option>)}
                         </Select>
                     </Form.Item>
                     <Form.Item
                         label="内容"
                         name="content"
                         rules={[{ required: true, message: '请输入文章内容' }]}
-                    ></Form.Item>
+                    >
+                        <ReactQuill
+                            className="publish-quill"
+                            theme="snow"
+                            placeholder="请输入文章内容"
+                        />
+                    </Form.Item>
 
                     <Form.Item wrapperCol={{ offset: 4 }}>
                         <Space>
